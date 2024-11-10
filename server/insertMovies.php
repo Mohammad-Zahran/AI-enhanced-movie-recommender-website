974c26eb062d71f25954e5253b0d030b1e21ac00
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-10 23:52:07 +0200

    Inserting data using foreach method

--- server/insertMovies.php
+++ server/insertMovies.php
@@ -2,25 +2,34 @@
 
 include "connection.php";
 
-$json_data = file_get_contents('movies.json');
+// Read the movies.json file and decode it into an array
+$json_data = file_get_contents('../movies.json');
 $movies = json_decode($json_data, true); 
 
-$title = $_POST["title"];
-$imageUrl = $_POST["imageUrl"];
-$genres = $_POST["genres"];
-$duration = $_POST["duration"];
-$movieLink = $_POST["movieLink"];
-$summary = $_POST["summary"]; 
-$numberOfLikes = $_POST["numberOfLikes"];
-$cast = $_POST["cast"];
-$releaseDate = $_POST["releaseDate"]; 
-$director = $_POST["director"]; 
+$query = $connection->prepare("INSERT INTO movies (movie_title, summary, cast, release_date, number_of_likes, genre, image, duaration, director, nationality) VALUES (?,?,?,?,?,?,?,?,?,?)");
 
-$query = $connection->prepare("INSERT INTO movies (movie_title, summary, cast, release_date, number_of_likes, genre, image, duaration, director) VALUES (?,?,?,?,?,?,?,?,?");
-$query->bind_param("ssssissss", $title, $summary, $cast, $releaseDate, $numberOfLikes,$genres,$imageUrl,$duration,$director);
-$query->execute();
-$result = $query->affected_rows;
+// I added a foreach to loop in all the json file content
+// You will see here that there are some with additionalData since some of those are inside an array
+foreach ($movies as $movie) {
+    $title = $movie['title'];
+    $summary = $movie['additionalData']['summary']; 
+    $cast = $movie['additionalData']['cast']; 
+    $releaseDate = $movie['additionalData']['releaseDate']; 
+    $numberOfLikes = $movie['additionalData']['numberOfLikes'];
+    $genres = $movie['genres'];
+    $imageUrl = $movie['imageUrl'];
+    $duration = $movie['duration'];
+    $director = $movie['additionalData']['directors']; 
+    $nationality = $movie['additionalData']['nationality'];
 
+    $query->bind_param("sssdisssss", $title, $summary, $cast, $releaseDate, $numberOfLikes, $genres, $imageUrl, $duration, $director, $nationality);
+    
+    if ($query->execute()) {
+        echo "Movie '$title' inserted successfully.<br>";
+    } else {
+        echo "Failed to insert the movie '$title'.<br>";
+    }
+}
 
 
 ?>
