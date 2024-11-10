cd5606c032ebde94a3ef0b938a096b0bb6884eea
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-11 00:41:49 +0200

    Fixed the format of the array and changed release_date to s

--- server/insertMovies.php
+++ server/insertMovies.php
@@ -6,13 +6,14 @@ include "connection.php";
 $json_data = file_get_contents('../movies.json');
 $movies = json_decode($json_data, true);
 
-// This is a simpler solution. It automatically skips rows that violate the UNIQUE constraint (like if the movie title already exists). The INSERT IGNORE statement will suppress the error, and no duplication will happen.
+// It automatically skips rows that violate the UNIQUE constraint (like if the movie title already exists). The INSERT IGNORE statement will suppress the error, and no duplication will happen.
 $query = $connection->prepare("INSERT IGNORE INTO movies (movie_title, summary, cast, release_date, number_of_likes, genre, image, duaration, director, nationality) VALUES (?,?,?,?,?,?,?,?,?,?)");
 
+// We used the foreach just to loop to each object inside the movies.json
 foreach ($movies as $movie) {
     $title = $movie['title'];
     $summary = $movie['additionalData']['summary']; 
-    $cast = isset($movie['additionalData']['cast']) ? implode(", ", $movie['additionalData']['cast']) : ''; 
+    $cast = isset($movie['additionalData']['cast']) ? implode(", ", $movie['additionalData']['cast']) : '';  // implode: takes the cast array and joins its elements into a single string, with each element separated by a comma and space (", ")
     $releaseDate = $movie['additionalData']['releaseDate']; 
     $numberOfLikes = $movie['additionalData']['numberOfLikes'];
     $genres = $movie['genres'];
@@ -21,7 +22,7 @@ foreach ($movies as $movie) {
     $director = $movie['additionalData']['directors']; 
     $nationality = $movie['additionalData']['nationality'];
 
-    $query->bind_param("sssdisssss", $title, $summary, $cast, $releaseDate, $numberOfLikes, $genres, $imageUrl, $duration, $director, $nationality);
+    $query->bind_param("ssssisssss", $title, $summary, $cast, $releaseDate, $numberOfLikes, $genres, $imageUrl, $duration, $director, $nationality);
     
     if ($query->execute()) {
         echo "Movie '$title' inserted successfully.<br>";
