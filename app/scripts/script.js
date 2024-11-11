9a5059e1638407aa2d2b55ef7a05e5a62e3ee8e5
FatimaAlZahraaHammoud <fatimaalzahraahammoud@gmail.com>
2024-11-11 18:02:14 +0200

    Style & Fix: Style and fix js code for movie section

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -8,12 +8,11 @@ class Movies{
         try{
             const response = await fetch(`${this.apiUrl}/getMovies.php`);
             if (!response.ok) throw new Error('Failed to fetch movies');
-            const rawResponse = await response.text();  // Log raw response as text
-            console.log(rawResponse);
+            const rawResponse = await response.text();
             const movies = JSON.parse(rawResponse);
             
-            const sixMovies = movies.slice(0, 6);
-            this.displayMovies(sixMovies)
+            const eightMovies = movies.slice(0, 8);
+            this.displayMovies(eightMovies)
         }
         catch(error){
             console.error("Error fetching data ", error);
@@ -26,23 +25,23 @@ class Movies{
             const movieCard = `
                 <div class="movie-card">
                     <img src="${movie.image}" alt="${movie.movie_title} poster" class="movie-poster">
+                    <span class="bookmark-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark">
+                            <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/>
+                        </svg>
+                    </span>
                     <div class="movie-details">
-                        <h4 class="movie-title">${movie.movie_title}</h4>
-                        <span class="movie-duration">${movie.duaration}</span>
-                        <div class="movie-icons">
+                        <div class="title-like-container">
+                            <h4 class="movie-title">${movie.movie_title}</h4>
                             <span class="like">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-thumbs-up like-btn">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-thumbs-up like-btn">
                                     <path d="M7 10v12"/>
                                     <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z"/>
                                 </svg>
                                 <p class="likes-number">${movie.number_of_likes}</p>
                             </span>
-                            <span class="bookmark-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark">
-                                    <path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/>
-                                </svg>
-                            </span>
                         </div>
+                        <span class="movie-duration">${movie.duaration}</span>
                         <div class="movie-rating">
                             <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
                         </div>
