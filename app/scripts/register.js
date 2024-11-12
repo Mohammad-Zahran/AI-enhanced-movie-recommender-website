beb13b433ea73f3c42a5715e577a29fe95062b91
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-12 03:27:31 +0200

    Enable asynchronous form submission with Axios for registration

---
+++ app/scripts/register.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('.form');
+const UsernameInput = document.getElementById('username');
+const EmailInput = document.getElementById('email');
+const PasswordInput = document.getElementById('password');
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault(); 
+
+    const data = new FormData();
+    data.append("username", UsernameInput.value);
+    data.append("email", EmailInput.value);
+    data.append("password", PasswordInput.value);
+
+    try {
+        const response = await axios({
+            method: "POST",
+            url: "http://localhost/FSW-SE-Factory/AI-enhanced-movie-recommender-website/server/register.php",
+            data: data, 
+        });
+
+        console.log(response.data); 
+
+        if (response.data.status === "Successful") {
+            alert("Registration Successful");
+        } else {
+            alert(response.data.message); 
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        alert("Failed to reach the server."); 
+    }
+});
