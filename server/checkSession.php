69642ba2ee0b6d1a28b58cc59c8e56ad288e0417
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-12 05:17:56 +0200

    Added checkSession API to check if the session occured

---
+++ server/checkSession.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if (isset($_SESSION['userId']) && isset($_SESSION['username'])) {
+    echo json_encode([
+        'status' => 'Logged in',
+        'userId' => $_SESSION['userId'],
+        'username' => $_SESSION['username']
+    ]);
+} else {
+    echo json_encode([
+        'status' => 'Not logged in',
+        'message' => 'User is not logged in'
+    ]);
+}
+?>
