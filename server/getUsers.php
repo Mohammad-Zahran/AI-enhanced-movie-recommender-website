4950311f662f40c38af6184c4cf09930c317ff51
Ahmad Ibrahim <99222724+AhmadIbrahim01@users.noreply.github.com>
2024-11-13 04:16:23 +0200

    Merge branch 'Fatima_AlZahraa_branch' into Ahmad's-Branch

--- server/getUsers.php
+++ server/getUsers.php
@@ -1,14 +1,14 @@
 <?php
 
-include "connection.php";
+    include "connection.php";
 
 
-$query = $connection->prepare("SELECT * FROM users WHERE role = 'admin'");
-    
-$query->execute();
-// Fetch results as an associative array
-$result = $query->get_result()->fetch_all(MYSQLI_ASSOC);
+    $query = $connection->prepare("SELECT * FROM users");
+        
+    $query->execute();
+    // Fetch results as an associative array
+    $result = $query->get_result()->fetch_all(MYSQLI_ASSOC);
 
-// We used this as to return as JSON ARRAY
-echo json_encode($result);
+    // We used this as to return as JSON ARRAY
+    echo json_encode($result);
 ?>
